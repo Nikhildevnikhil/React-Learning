5d78e3021b23a26d65263b6fc8ec0aafc94999ff
nikhildevnikhil <nikhildeonikhil@gmail.com>
2023-11-26 01:20:14 +0530

    mini context

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import UserContextProvider from "./Context/UserContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+
+function App() {
+  
+  return (
+    
+      <UserContextProvider>
+        <h1>Context Provider</h1>
+        <Login />
+        <Profile />
+      </UserContextProvider>
+    
+  );
+}
+
+export default App
